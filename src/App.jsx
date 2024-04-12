779785e98e40229c7f13f8731920bc1f31999713
Bruno P <brunopalavecino62@gmail.com>
2024-04-12 18:24:46 -0300

    First steps

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  return (
+    <main className='board'>
+      <h1>Tic-tac-toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+
+              </Square>
+            )
+
+          })
+        }
+      </section>
+    </main>
+
+  )
+}
+
+export default App
