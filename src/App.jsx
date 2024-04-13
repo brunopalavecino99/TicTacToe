e107a9bc846558ff6a6d8e41c26cf72ddd8325fe
Bruno P <brunopalavecino62@gmail.com>
2024-04-13 13:35:18 -0300

    Add Turns

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ const TURNS = {
 
 
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
   return (
-    <div className='square'>
+    <div className={className}>
       {children}
     </div>
   )
@@ -20,6 +21,7 @@ const Square = ({ children, updateBoard, index }) => {
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
 
   return (
     <main className='board'>
@@ -39,7 +41,15 @@ function App() {
           })
         }
       </section>
-    </main>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O} >
+          {TURNS.O}
+        </Square>
+      </section>
+    </main >
 
   )
 }
